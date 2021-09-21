7e77b36701ba5d30f9dbb5a4538baba3b8cd3569
Hubert Hickman <hickmanhb@nih.gov>
2021-09-21 07:51:01 -0500

    Hopefully really fixed now -- make the trial selection pay attention to the tokenized date.

--- sec_poc_classifier.py
+++ sec_poc_classifier.py
@@ -76,24 +76,23 @@ con = sqlite3.connect(args.dbfilename)
 
 
 get_trials_sql = """
-select t.nct_id , t.record_verification_date, t.amendment_date, td.classification_date from trials t 
+select t.nct_id , t.record_verification_date, t.amendment_date,td.tokenized_date, td.classification_date from trials t 
 left outer join trial_nlp_dates td on t.nct_id = td.nct_id 
-where td.classification_date is null or td.classification_date <= max(coalesce(t.record_verification_date,'1980-01-01'),
-                                                                      coalesce(t.amendment_date,'1980-01-01'))
+where td.classification_date is null or td.classification_date <= td.tokenized_date
 """
 
 cur = con.cursor()
 cur.execute(get_trials_sql)
 trials_to_classify = cur.fetchall()
 print("there are ", len(trials_to_classify), " trials to classify ")
-print(f"{'Count' : <8}{'  NCT ID': <15}{'RVD' : ^30}{'Amendment Date' : ^30}{'Prior Classification Date' : ^30}")
+print(f"{'Count' : <8}{'  NCT ID': <15}{'RVD' : ^30}{'Amendment Date' : ^30}{'Tokenized Date' : ^30}{'Prior Classification Date' : ^30}")
 #sys.exit()
 #cur.execute('delete from candidate_criteria')
 con.commit()
 i = 1
 for trial in trials_to_classify:
     nct_id = trial[0]
-    print(f"{i+1: <8}{trial[0]: <15}{trial[1] or '': ^30}{trial[2] or '': ^30}{trial[3] or '': ^30}")
+    print(f"{i+1: <8}{trial[0]: <15}{trial[1] or '': ^30}{trial[2] or '': ^30}{trial[3] or '': ^30}{trial[4] or '': ^30}")
 
     cur.execute("delete from candidate_criteria where nct_id = ? ", [nct_id])
 
