8c78cc1acf7aa3e91d9383aab2137e2c000731ed
Hubert Hickman <hickmanhb@nih.gov>
2022-02-18 02:50:59 -0600

    Initial checkin of config file for criteria_types

---
+++ common_funcs.py
@@ -0,0 +1,14 @@
+def get_criteria_type_map():
+    d = {
+        'biomarker_exc' : 1,
+        'biomarker_inc' : 2,
+        'hiv_exc' : 5,
+        'plt': 6,
+        'wbc': 7,
+        'perf': 8,
+        'bmets': 35,
+        'pt_inc': 36,
+        'pt_exc': 37,
+        'disease_inc' : 38
+    }
+    return d
