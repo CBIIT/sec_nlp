9d43196e13b55cc9eb823da04a1611fe38abc5da
crouchcd <cameroncrouch36@gmail.com>
2025-02-20 19:13:58 +0000

    add scripts

---
+++ scripts/etl_script.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+python sec_poc_tokenizer.py --dbname sec --host r_363-postgres-1 --user secapp --port 5432
+python sec_poc_classifier.py --dbname sec --host r_363-postgres-1 --user secapp --port 5432
+python sec_poc_expression_generator.py --dbname sec --host r_363-postgres-1 --user secapp --port 5432
