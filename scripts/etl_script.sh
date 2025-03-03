d49765c11884ce5e82f2a4a1a558bcaf90c26b1d
crouchcd <cameroncrouch36@gmail.com>
2025-03-03 16:32:27 -0600

    update etl script hostname

--- scripts/etl_script.sh
+++ scripts/etl_script.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-python sec_poc_tokenizer.py --dbname sec --host r_363-postgres-1 --user secapp --port 5432
-python sec_poc_classifier.py --dbname sec --host r_363-postgres-1 --user secapp --port 5432
-python sec_poc_expression_generator.py --dbname sec --host r_363-postgres-1 --user secapp --port 5432
+python sec_poc_tokenizer.py --dbname sec --host oel8-v2-postgres-1 --user secapp --port 5432
+python sec_poc_classifier.py --dbname sec --host oel8-v2-postgres-1 --user secapp --port 5432
+python sec_poc_expression_generator.py --dbname sec --host oel8-v2-postgres-1 --user secapp --port 5432
